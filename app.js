4b53a1ed2d8cb9eb50db0f2795ee3afd0ef84853
lagun4ik <ivan.ofical@gmail.com>
2015-06-08 20:17:27 +0300

    add debug

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 var config = require("nconf");
 
-config.argv()
-    .file({
-        file: 'config.json'
-    });
+config.argv().file({file: 'config.json'});
 
 var 
-	db       = require('./core/db'),
 	app      = require('./core/express'),
-	passport = require('./core/passport'),
-	http     = require('http').createServer(app),
-	socket   = require('./core/socket')(http),
+	server   = require('http').createServer(app),
+	socket   = require('./core/socket')(server),
+	debug    = require('debug')('http');
 	port     = config.get("app:port") || 3000;
 
-
 app.set('port', port);
-http.listen(port);
+server.listen(port);
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+
+function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
