31ffc354b5235c9c8e55f45ed790af86b0889d76
lagun4ik <ivan.ofical@gmail.com>
2015-06-08 03:13:23 +0300

    Migration to bower

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,8 @@ var
     csso = require('gulp-csso'),
     sourcemaps = require('gulp-sourcemaps'),
     myth = require('gulp-myth'),
-    gutil = require('gulp-util'),
-    source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
-    browserify = require('browserify'),
+    mainBowerFiles = require('main-bower-files'),
+    concat = require('gulp-concat'),
     package = require('./package.json');
 
 
@@ -31,75 +29,73 @@ var banner = [
 gulp.task('css', function() {
     gulp.src('./assets/scss/style.scss')
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole: true
-        }))
+        .pipe(sass({errLogToConsole: true}))
         .pipe(myth())
         .pipe(csso())
-        .pipe(header(banner, {
-            package: package
-        }))
+        .pipe(header(banner, {package: package}))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./public/css'))
+        .pipe(gulp.dest('./public/css/'))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
 gulp.task('js', function() {
-    browserify('./assets/js/app.js', {
-            debug: true
-        })
-        .bundle()
-        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-        .pipe(source('app.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({
-            loadMaps: true
-        }))
+  gulp.src(['./assets/js/states/*.js', './assets/js/entities/*.js', './assets/js/helpers/*.js', './assets/js/app.js'])
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(header(banner, {package: package}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('bower', function() {
+    gulp.src(mainBowerFiles())
+        .pipe(concat('libs.js'))
         .pipe(uglify())
-        .pipe(header(banner, {
-            package: package
-        }))
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./public/js/'));
 });
 
 var BROWSER_SYNC_RELOAD_DELAY = 500;
-
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
-    script: 'app.js',
-    ignore: ['assets/', 'public/', 'node_modules/']
-  })
-    .on('start', function onStart() {
-      if (!called) { cb(); }
-      called = true;
-    })
-    .on('restart', function onRestart() {
-      setTimeout(function reload() {
-        browserSync.reload({
-          stream: false
+gulp.task('nodemon', function(cb) {
+    var called = false;
+    return nodemon({
+            script: './app.js',
+            ignore: ['assets/', 'public/', 'node_modules/']
+        })
+        .on('start', function onStart() {
+            if (!called) {
+                cb();
+            }
+            called = true;
+        })
+        .on('restart', function onRestart() {
+            setTimeout(function reload() {
+                browserSync.reload({
+                    stream: false
+                });
+            }, BROWSER_SYNC_RELOAD_DELAY);
         });
-      }, BROWSER_SYNC_RELOAD_DELAY);
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init({
+        files: ['./views/**/*.hbs'],
+        proxy: 'http://localhost:3000',
+        port: 4000,
+        open: false
     });
 });
 
-gulp.task('browser-sync', ['nodemon'], function () {
-  browserSync.init({
-    files: ['public/js/*.*', 'views/**/*.*'],
-    proxy: 'http://localhost:3000',
-    port: 4000,
-    open: false
-  });
+gulp.task('bs-reload', function () {
+  browserSync.reload();
 });
 
 
-
-gulp.task('default', ['browser-sync', 'css', 'js'],
+gulp.task('default', ['browser-sync', 'bower', 'css', 'js'],
     function() {
         gulp.watch("./assets/scss/**/*.scss", ['css']);
-        gulp.watch("./assets/js/**/*.js", ['js']);
+        gulp.watch("./assets/js/**/*.js", ['js', 'bs-reload']);
     }
 );
