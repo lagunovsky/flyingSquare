f6d83143ea7429f19b158b5064312868054038ae
lagun4ik <ivan.ofical@gmail.com>
2015-06-09 05:45:55 +0300

    add game core

--- core/game.js
+++ core/game.js
@@ -1,9 +1,25 @@
-var config = require("nconf").argv().file({file: 'config.json'});
+var
+  config = require("nconf").argv().file({file: 'config.json'}),
+  debug = require("debug")('game');
 
 var game = {
-  players: [],
-  addPlayer: function () {
-    console.log(game.players.lenght);
+  players: {
+    count: -1
+  },
+  addPlayer: function (id) {
+    game.players.count++;
+    if (game.players.count < 4) {
+      game.players[id] = {
+        color: game.getPlayerColor(game.players.count)
+      }
+    }
+    debug('add %s; players: %o', id, game.players);
+    return id;
+  },
+  delPlayer: function(id){
+    game.players.count--;
+    delete(game.players[id]);
+    debug('del %s; players: %o', id, game.players);
   },
   getPlayerColor: function (n) {
     return config.get("game:colors")[n];
