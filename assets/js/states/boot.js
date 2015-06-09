61f0ed020115c42f5109d51cdb8d805fc8051759
lagun4ik <ivan.ofical@gmail.com>
2015-06-09 05:44:03 +0300

    Configuring the connection sockets

--- assets/js/states/boot.js
+++ assets/js/states/boot.js
@@ -1,20 +1,22 @@
-var Game = {};
+var Game = {
+  Socket: io('localhost:3000'),
+  Players: [],
+  Player: {}
+};
 
 Game.Boot = {
-    init: function () {
-        this.game.stage.backgroundColor = '#e2e2e2';
-        this.input.maxPointers = 1;
-        this.stage.disableVisibilityChange = true;
-    },
-    preload: function() {
-        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.scale.setScreenSize(true);
-        this.load.image('progressBar', '/gfx/progress_bar.png');
-    },
-    create: function() {
-        this.physics.startSystem( Phaser.Physics.ARCADE );
-        this.state.start( 'Preloader' );
-    }
-
-
+  init: function () {
+    this.game.stage.backgroundColor = '#e2e2e2';
+    this.input.maxPointers = 1;
+    this.stage.disableVisibilityChange = true;
+  },
+  preload: function () {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.setScreenSize(true);
+    this.load.image('progressBar', '/gfx/progress_bar.png');
+  },
+  create: function () {
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.state.start('Preloader');
+  }
 };
